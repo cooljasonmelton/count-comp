51daf7c8efd759ac22a47c4a452512caca19faa1
cooljasonmelton <cooljasonmelton>
2020-10-17 21:01:05 -0500

    abstracting count comp

--- src/components/Count.js
+++ src/components/Count.js
@@ -3,26 +3,22 @@ import React, { useEffect, useState } from 'react';
 //styling
 import './Count.css';
 
-const Count = () => {
-  const [number, setNumber] = useState(0)
+const Count = props => {
+  const {label, number, duration } = props.data
+  const [count, setCount] = useState(0)
 
   useEffect(() => {
-      const countUp = () => {
-      let start = 0;
-      let end = 0;
-      if (start === end) return;
-      let timer = setInterval(() => {
-        start += 1;
-        if (start === end) clearInterval(timer)       
-        setNumber(start)
-      }, (1000))
-    }
-    countUp()
+    let end = parseInt(number)
+    if (count === end) return;
+    let timer = setInterval(() => {
+      if (count === end) clearInterval(timer)       
+      setCount(count + 1)
+    }, (50))
   }, []);
 
   return (
     <div className="Count">
-      <h3> <i>{counts[0].label}: {number}</i></h3>
+      <h3><i>{label}: {count}</i></h3>
     </div>
   );
 }
