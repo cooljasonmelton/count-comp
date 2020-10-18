bba83cbfc8f34e8a6c19a588bb77ca77edbc395a
cooljasonmelton <cooljasonmelton>
2020-10-17 22:02:07 -0500

    finished func and added demo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # count-comp
+
+<img src="" width="50%" alt="count-comp-demo">
--- src/components/Count.js
+++ src/components/Count.js
@@ -4,17 +4,36 @@ import React, { useEffect, useState } from 'react';
 import './Count.css';
 
 const Count = props => {
+  // label of counter
+  // number to increment up to
+  // duration of counter in seconds
   const {label, number, duration } = props.data
   const [count, setCount] = useState(0)
 
   useEffect(() => {
-    let end = parseInt(number)
-    if (count === end) return;
+    let start = 0;
+    // first three numbers from props
+    const end = parseInt(number.substring(0,3))
+    // if zero, return
+    if (start === end) return;
+
+    // find duration per increment
+    let totalMilSecDur = parseInt(duration);
+    let incrementTime = (totalMilSecDur / end) * 1000;
+
+    console.log(end)
+
+    // timer increments start counter 
+    // then updates count
+    // ends if start reaches end
     let timer = setInterval(() => {
-      if (count === end) clearInterval(timer)       
-      setCount(count + 1)
-    }, (50))
-  }, []);
+      start += 1;
+      setCount(String(start) + number.substring(3))
+      if (start === end) clearInterval(timer)       
+    }, incrementTime);
+
+    // dependency array
+  }, [number, duration]);
 
   return (
     <div className="Count">
